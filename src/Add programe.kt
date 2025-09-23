61e3d59c49f2c5f774a58a6eb64bc50ca9dcfcd3
Aamer <asmehdi110@gmail.com>
2025-09-23 10:22:59 +0500

    Subtraction added

--- src/Add programe.kt
+++ src/Add programe.kt
@@ -1,6 +1,7 @@
 fun main() {
-    val a: Int = 5   // integer variable a with value 5
+    val a: Int = 10   // integer variable a with value 5
     val b: Int = 6   // integer variable b with value 6
     val sum = a + b  // add a and b, store in sum
-    println("Sum : $sum") // print the result
+    val subtract = a - b
+    println("Sum : $sum and Subtraction : $subtract") // print the result
 }
