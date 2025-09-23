322ae3df0bea1174032dae50de693f01f2bd26ee
Aamer <asmehdi110@gmail.com>
2025-09-23 10:17:13 +0500

    New Kotlin file added

---
+++ src/Add programe.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val a: Int = 5   // integer variable a with value 5
+    val b: Int = 6   // integer variable b with value 6
+    val sum = a + b  // add a and b, store in sum
+    println("Sum : $sum") // print the result
+}
